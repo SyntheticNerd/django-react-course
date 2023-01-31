71dc853fbdd99e96615e8faf924c343bfcf4f4db
SyntheticDesigner <andrew.sch.j@gmail.com>
2023-01-31 13:45:48 -0800

    refining the api views

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,16 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 from .products import products
 
 # Create your views here.
 
-
+@api_view('GET', '')
 def getRoutes(request):
     return JsonResponse('Hello', safe=False)
 
